d7a719bfff90caf3e1c77dad3f459d89e57c38d3
Konrad Malik <konrad.malik@gmail.com>
2024-12-01 13:10:12 +0100

    1 part 1

--- 1/go.mod
+++ 1/go.mod
@@ -1,3 +1,7 @@
+replace github.com/konradmalik/aoc2024/lib => ../glib
+
 module github.com/konradmalik/aoc2024
 
 go 1.23
+
+require github.com/konradmalik/aoc2024/lib v0.0.0-00010101000000-000000000000
--- 1/main.go
+++ 1/main.go
@@ -1,7 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"log"
+	"os"
+	"slices"
+
+	"github.com/konradmalik/aoc2024/lib"
+)
+
+const InputFile string = "./input.txt"
+
+func readInput(file *os.File) [][]int {
+	rows := make([][]int, 2)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		row := lib.ParseNumbers(scanner.Text())
+		rows[0] = append(rows[0], row[0])
+		rows[1] = append(rows[1], row[1])
+	}
+	err := scanner.Err()
+	if err != nil {
+		log.Fatalf("scanner encountered an err: %s", err)
+	}
+
+	return rows
+}
+
+func distances(a, b []int) []int {
+	ds := make([]int, len(a))
+	for i, xa := range a {
+		xb := b[i]
+
+		d := xa - xb
+		if d < 0 {
+			d = -d
+		}
+		ds[i] = d
+	}
+	return ds
+}
+
+func sum(a []int) int {
+	sum := 0
+	for _, x := range a {
+		sum += x
+	}
+	return sum
+}
 
 func main() {
-	fmt.Println("yo")
+	file, err := os.Open(InputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	columns := readInput(file)
+	slices.Sort(columns[0])
+	slices.Sort(columns[1])
+
+	distances := distances(columns[0], columns[1])
+
+	sum := sum(distances)
+
+	log.Println(sum)
 }
